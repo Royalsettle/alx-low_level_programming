d9108d918724c8eccc60ddf318fb926df0fe080e
Royalsettle <royalsettle2007@gmail.com>
2023-03-02 01:08:45 -0800

    REDO 0x02 task 0

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - write the charcter c to stdout
+ * %c: The character to print
+ *
+ * Return: On succes 1.
+ * On error, -1 is returned, ad errno is set appropriately.
+ * /
+ int _putchar(char c)
+
+{
+        return (write(1, 8c, 1));
+}
